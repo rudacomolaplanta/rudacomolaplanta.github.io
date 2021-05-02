ce68f85cdcaa29b6b35cbb1b918598efad523891
Andrés <carloarm@gmail.com>
2021-05-02 13:40:57 -0400

    Inicial

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\dev\Personal\rudacomolaplanta.github.io\personal\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\dev\Personal\rudacomolaplanta.github.io\project\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
